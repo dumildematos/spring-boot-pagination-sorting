b4305433eb7528a901136d827100580e1c077bed
Dumilde Matos <dumildejosematos@gmail.com>
2021-10-10 01:14:39 +0100

    firt commit

---
+++ src/test/java/com/example/springbootpaginationsorting/SpringBootPaginationSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpaginationsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaginationSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
